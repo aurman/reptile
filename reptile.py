e77ada9e696b9749b21244398d723de20e355076
Chris Van Horne <chris.vanhorne@gmail.com>
2013-08-08 18:05:31 -0700

    HTTP 301 to black tile on out-of-bounds imagery. Fix #1

--- reptile.py
+++ reptile.py
@@ -1,14 +1,24 @@
 from __future__ import print_function
 
 import os.path
+import random
 import subprocess
 import sys
+from contextlib import contextmanager
 
 import flask
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
 import config
+
+
+@contextmanager
+def ignored(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
 
 tile_shell_cmd = '''
 mkdir -p {tile_filename}/{z}/{x} ; \
@@ -59,7 +69,19 @@ if not app.config.jp2_root:
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    sources = [
+        ('Fading Rover Tracks Near Victoria Crater', '/PDS/EXTRAS/RDR/ESP/ORB_013900_013999/ESP_013954_1780/ESP_013954_1780_RED.QLOOK.JP2'),
+        ('Continued Monitoring of Slope Streaks in Olympus Mons Aureole', '/PDS/EXTRAS/RDR/ESP/ORB_012200_012299/ESP_012258_2080/ESP_012258_2080_RED.QLOOK.JP2'),
+        ('Meander and Tributary of Scamander Vallis', '/PDS/EXTRAS/RDR/ESP/ORB_011200_011299/ESP_011289_1950/ESP_011289_1950_RED.QLOOK.JP2'),
+        ('Crater in Meridiani Planum with Layering', '/PDS/EXTRAS/RDR/ESP/ORB_011200_011299/ESP_011277_1825/ESP_011277_1825_RED.QLOOK.JP2'),
+    ]
+    with ignored(IOError):
+        with open('../jp2_index') as f:
+            for n, line in enumerate(f):
+                if random.randrange(n + 2):
+                    continue
+                sources.append(line.split('='))
+    return render_template('index.html', sources=sources)
 
 
 @app.route('/<path:filename>.JP2')
@@ -84,7 +106,7 @@ def tile(filename, z, x, y):
             z=z, x=x, y=y, level=10-z,
             tile_x=x*tilesize, tile_y=y*tilesize, tilesize=tilesize)
         if subprocess.call(cmd, shell=True) != 0:
-            return ''
+            return flask.redirect('/static/black.png')
     return flask.send_file(tile, mimetype='image/png')
 
 if __name__ == '__main__':
